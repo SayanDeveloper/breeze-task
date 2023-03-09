35420ec9640923f787332e8cab6397b9993ae285
Sayan Samanta <samantasayan23@gmail.com>
2023-03-10 03:27:08 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
       <main>
         hello world!
-        
+        <Link href="/sign-in">Sign In</Link>
       </main>
     </>
   )
